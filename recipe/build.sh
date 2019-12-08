36c153432de11117685fb05b7584fb615a38110c
markklik <markklik@gmail.com>
2019-12-08 22:22:03 +0100

    Overrides for Clang compiler

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]] || [[ $target_platform == win-64 ]] || [[ $target_platform == osx-64 ]]; then
   export DISABLE_AUTOBREW=1
+  echo show environment
+  set
+  printenv
+  echo clang overrides
+  export CXX=$CLANGXX
+  export CC=$CLANG
+   
   $R CMD INSTALL --build .
 else
   mkdir -p $PREFIX/lib/R/library/fst
